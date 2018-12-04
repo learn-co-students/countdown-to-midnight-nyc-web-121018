c83553c46aa1d4e1c1d29573564a8a240c39a1f9
powell956 <powell956@gmail.com>
2018-12-04 01:26:54 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown steps
+  while steps > 0
+    puts "#{steps} SECOND(S)!"
+    steps -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep seconds
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
